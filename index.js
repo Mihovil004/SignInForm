62727d4a04bec9c0b7cdb6bdf15673920ff1de0c
Mihovil004 <mihovilsunara0@gmail.com>
2025-02-06 23:08:10 +0100

    Refactor login functionality to use AJAX and enhance UI design for the login page

--- index.js
+++ index.js
@@ -64,8 +64,21 @@ app.get("/", (req, res) => {
   res.render("index");
 });
 
-app.post("/login", passport.authenticate("local"), (req, res) => {
-  res.status(200).send("Login successful");
+app.post("/ajax-login", (req, res, next) => {
+  passport.authenticate("local", (err, user, info) => {
+    if (err) {
+      return res.status(500).json({ message: "Internal Server Error" });
+    }
+    if (!user) {
+      return res.status(401).json({ message: info.message });
+    }
+    req.logIn(user, (err) => {
+      if (err) {
+        return res.status(500).json({ message: "Internal Server Error" });
+      }
+      return res.status(200).json({ message: "Login successful" });
+    });
+  })(req, res, next);
 });
 
 app.use((req, res, next) => {
