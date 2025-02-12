13cbd6b18575344bbf00bfbcbe9fc009424ac5fc
Mihovil004 <mihovilsunara0@gmail.com>
2025-02-12 11:16:13 +0100

    Add session secret configuration and environment variables for improved security

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
+const sessionSecret = process.env.SESSION_SECRET || "secret";
 app.use(express.static(path.join(__dirname, "public")));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -20,7 +21,7 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(
   session({
-    secret: process.env.SESSION_SECRET || "secret",
+    secret: sessionSecret,
     resave: false,
     saveUninitialized: false,
   })
